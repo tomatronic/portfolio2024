efaf4b8cf697c1f6e3fd5611da2ffa7f90285c9f
Tom Spencer <thomasspencer@Thomass-Mac-mini.local>
2024-11-27 14:21:48 +0000

    Adding some fixes to the navigation bar

--- src/app/components/examples.js
+++ src/app/components/examples.js
@@ -7,13 +7,13 @@ export default function ExampleGallery() {
     return (
         <div>
         <div className="pt-16 md:pt-16">
-          <div className="container mx-auto max-w-screen-xl">
+          <div className="container mx-auto max-w-screen-lg">
           <h1 className="text-center text-2xl font-extrabold leading-none tracking-tight text-gray-100 md:text-3xl xl:text-4xl">Extra Pixels</h1>
           <p className="mx-auto max-w-xl text-balance pb-16 pt-4 text-center text-base text-gray-100 dark:text-gray-400">A collection of personal projects.</p>
 <div className="grid grid-cols-2 gap-4 px-4 pb-16 md:grid-cols-4">
     <div className="grid gap-4">
         <div>        
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg" alt="" />
+            <img className="h-auto max-w-full rounded-lg" src="https://placehold.co/236x300" alt="" />
         </div>
         <div>
             <Zoom>
@@ -21,15 +21,15 @@ export default function ExampleGallery() {
             </Zoom>
         </div>
         <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg" alt="" />
+            <img className="h-auto max-w-full rounded-lg" src="https://placehold.co/236x236" alt="" />
         </div>
     </div>
     <div className="grid gap-4">
         <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg" alt="" />
+            <img className="h-auto max-w-full rounded-lg" src="https://placehold.co/236x236" alt="" />
         </div>
         <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg" alt="" />
+            <img className="h-auto max-w-full rounded-lg" src="https://placehold.co/236x300" alt="" />
         </div>
         <div>
         <Zoom>
@@ -39,18 +39,18 @@ export default function ExampleGallery() {
     </div>
     <div className="grid gap-4">
         <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg" alt="" />
+            <img className="h-auto max-w-full rounded-lg" src="https://placehold.co/236x300" alt="" />
         </div>
         <Zoom>
                 <Image src="/todoList.gif" alt="Tom Spencer - Senior UX Designer" width={1000} height={728} className="h-auto max-w-full rounded-lg" />
             </Zoom>
         <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg" alt="" />
+            <img className="h-auto max-w-full rounded-lg" src="https://placehold.co/236x236" alt="" />
         </div>
     </div>
     <div className="grid gap-4">
         <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg" alt="" />
+            <img className="h-auto max-w-full rounded-lg" src="https://placehold.co/236x236" alt="" />
         </div>
         <div>
         <Zoom>
@@ -58,7 +58,7 @@ export default function ExampleGallery() {
             </Zoom>
         </div>
         <div>
-            <img className="h-auto max-w-full rounded-lg" src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg" alt="" />
+            <img className="h-auto max-w-full rounded-lg" src="https://placehold.co/236x175" alt="" />
         </div>
     </div>
 </div>
