f6fcb762bd917859096001543e6553ddf17897a6
Tom Spencer <thomasspencer@Thomass-Mac-mini.local>
2024-11-27 14:50:32 +0000

    Adding some fixes to the navigation bar

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,34 +1,18 @@
-import localFont from "next/font/local";
 import "./globals.css";
 import Navigation from './components/navigation';
 import Footer from "./components/footer";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import Head from "next/head";
 
-
-
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
-export async function generateMetadata() {
-  return {
-    title: "Tom Spencer - UX Designer",
-    description: "Hello, I am Tom Spencer, a Senior UX Designer based in Brighton, UK. I am currently working at Rakuten Advertising.",
-    }
-}
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">      
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+    <html lang="en">  
+    <Head>
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet" />
+    </Head>    
+      <body>
         <Navigation />
         {children}
         <Footer />
