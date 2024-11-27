9476da40ad78c165c624d4892e2e08ce5c744451
Tom Spencer <thomasspencer@Thomass-Mac-mini.local>
2024-11-27 15:32:52 +0000

    Trying to render fonts correctly/better

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,22 +3,27 @@ import Navigation from './components/navigation';
 import Footer from "./components/footer";
 import { GoogleAnalytics } from "@next/third-parties/google";
 import Head from "next/head";
+import { Open_Sans } from 'next/font/google';
 
+// Optimize font loading
+const openSans = Open_Sans({ 
+  subsets: ['latin'],
+  display: 'swap' 
+});
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">  
-    <Head>
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet" />
-    <p>Test</p>
-    </Head>    
+    <html lang="en" className={openSans.className}>  
+      <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+      </Head>
       <body>
         <Navigation />
         {children}
         <Footer />
+        <GoogleAnalytics gaId="G-CCDKVM70NV" debug={true} />
       </body>
-      <GoogleAnalytics gaId="G-CCDKVM70NV" debug= {true} />
     </html>
   );
 }
