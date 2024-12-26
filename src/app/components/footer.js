e776b29e1cc7a51e21502a8d3c7856cdd5cd9ec7
Tom Spencer <thomasspencer@Thomass-Mac-mini.local>
2024-12-26 19:44:37 +0000

    Minor update to about page and smaller footer copy

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -4,8 +4,8 @@ export default function Footer() {
     return (
 <div className="flex w-full items-center justify-center bg-slate-950">
         <div className="flex w-full flex-col px-4 md:w-2/3">                       
-            <div className="flex flex-col">                               
-                <p className="my-12 w-full text-center text-gray-100">Designed and built by Tom Spencer - {year}</p>
+            <div className="text-xs flex flex-col">                               
+                <div className="text-xs my-12 w-full text-center text-gray-100">Designed and built by Tom Spencer - {year}</div>
             </div>
         </div>
     </div>
