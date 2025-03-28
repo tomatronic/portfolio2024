5511fdc679373d69874141505776cca3fe48a129
Tom Spencer <tom.m.spencer@gmail.com>
2025-03-28 10:35:18 +0000

    Offer thumbnail updated and copy amends

--- src/app/components/casestudyShowcase.js
+++ src/app/components/casestudyShowcase.js
@@ -7,20 +7,20 @@ function CasestudyShowcase() {
   return (
     <div id="work">
       <div className="py-24">
-        <div className="container mx-auto max-w-screen-lg px-4">          
+        <div className="container mx-auto max-w-screen-lg px-4">
           <div className="space-y-16">
-            <div className="grid grid-cols-1 gap-8 rounded-2xl bg-purple-100 md:grid-cols-2 md:gap-20">
-              <div className="relative grid grid-cols-1 content-end overflow-visible px-8 py-8 md:px-0 md:py-0 md:pl-20 md:pt-20">
-                <div className="relative inset-0 overflow-hidden md:absolute">
-                  <Image src="/offersThumb.png" alt="Streamlining the offer management experience for Rakuten Advertising" className="left-1/2 top-1/2 min-h-full min-w-full md:pl-8 md:pt-8" layout="fill" objectFit="cover" priority={true} />
+          <div className="grid grid-cols-1 gap-8 rounded-2xl bg-purple-100 md:grid-cols-2 md:gap-20">
+          
+                <div className="grid-col-1 flex content-center justify-end overflow-hidden">
+                  <Image src="/offersThumb.png" alt="Streamlining the offer management experience for Rakuten Advertising" width={0} height={0} sizes="100vw" style={{ width: '150%', height: 'auto', maxWidth: '1200px', objectFit: "contain" }} />
                 </div>
-              </div>
+              
 
               <div className="grid grid-cols-1 justify-center gap-4 px-8 py-8 md:px-0 md:py-20 md:pr-20">
                 <div className="text-sm font-medium uppercase text-gray-600">Rakuten Advertising</div>
                 <h2 className="text-3xl font-semibold tracking-tight text-gray-700">Enhancing offer management</h2>
                 <p className="pb-4 text-base font-medium text-gray-700 dark:text-gray-400">
-                  Reimagining how users manage offers by creating a more intuitive and efficient experience. I crafted a solution that simplifies workflows and elevates visibility.
+                  Reimagining how users manage offers. I crafted a solution that simplifies workflows and elevates visibility.
                 </p>
                 <Link href="casestudy/Rakuten" className="self-start">
                   <button className="rounded-xl border border-gray-300 bg-white/65 px-8 py-4 text-base font-medium text-gray-600 shadow-md transition-all hover:bg-white/35 hover:shadow-lg" type="button">
@@ -30,27 +30,27 @@ function CasestudyShowcase() {
               </div>
             </div>
             <div className="grid grid-cols-1 gap-8 rounded-2xl bg-yellow-50 md:grid-cols-2 md:gap-20">
-            <div className="grid-col-1 relative grid content-center overflow-visible md:order-last md:pl-20">
-  <div className="relative inset-0 h-[20rem] overflow-hidden md:absolute md:h-full">
-    <Image 
-      src="/sm_tools.png" 
-      alt="Creating a tool hire app in 48 hours" 
-      layout="fill"
-      objectFit="cover"
-      className="absolute left-1/2 top-1/2 min-h-full min-w-full"
-    />
-  </div>
-</div>
+              <div className="grid-col-1 relative grid content-center overflow-visible md:order-last md:pl-20">
+                <div className="relative inset-0 h-[20rem] overflow-hidden md:absolute md:h-full">
+                  <Image
+                    src="/sm_tools.png"
+                    alt="Creating a tool hire app in 48 hours"
+                    layout="fill"
+                    objectFit="cover"
+                    className="absolute left-1/2 top-1/2 min-h-full min-w-full"
+                  />
+                </div>
+              </div>
 
               <div className="grid grid-cols-1 justify-center gap-4 px-8 py-8 md:px-0 md:py-20 md:pl-20">
                 <div className="text-sm font-medium uppercase text-gray-600">Designflows 2021</div>
                 <h2 className="text-3xl font-semibold tracking-tight text-gray-700">Creating a tool hire app in 48 hours</h2>
                 <p className="pb-4 text-base font-medium text-gray-700 dark:text-gray-400">
-                A fast-paced design sprint from concept to high-fidelity mocks—testing my skills, creativity, and adaptability under pressure.
+                  A fast-paced design sprint from concept to high-fidelity mocks—testing my skills, creativity, and adaptability under pressure.
                 </p>
                 <Link href="/casestudy/DesignFlows" className="self-start">
-                <button className="rounded-xl border border-gray-300 bg-white/65 px-8 py-4 text-base font-medium text-gray-600 shadow-md transition-all hover:bg-white/35 hover:shadow-lg" type="button">
-                Read case study
+                  <button className="rounded-xl border border-gray-300 bg-white/65 px-8 py-4 text-base font-medium text-gray-600 shadow-md transition-all hover:bg-white/35 hover:shadow-lg" type="button">
+                    Read case study
                   </button>
                 </Link>
               </div>
@@ -62,19 +62,19 @@ function CasestudyShowcase() {
 
               <div className="grid grid-cols-1 justify-center gap-4 px-8 py-8 md:px-0 md:py-20 md:pr-20">
                 <div className="text-sm font-medium uppercase text-gray-600">Brewtiful</div>
-                <h2 className="text-3xl font-semibold tracking-tight text-gray-700">Creating an engaging craft beer brand and blog</h2>
+                <h2 className="text-3xl font-semibold tracking-tight text-gray-700">Creating an engaging craft beer brand &amp; blog</h2>
                 <p className="pb-4 text-base font-medium text-gray-700 dark:text-gray-400">
-                A passion project taking a craft beer community blog through the full UX process — I still hope to bring this to life one day.
+                  A passion project taking a craft beer community blog through the full UX process — I still hope to bring this to life one day.
                 </p>
                 <Link href="/casestudy/Brewtiful" className="self-start">
-                <button className="rounded-xl border border-gray-300 bg-white/65 px-8 py-4 text-base font-medium text-gray-600 shadow-md transition-all hover:bg-white/35 hover:shadow-lg" type="button">
-                Read case study
+                  <button className="rounded-xl border border-gray-300 bg-white/65 px-8 py-4 text-base font-medium text-gray-600 shadow-md transition-all hover:bg-white/35 hover:shadow-lg" type="button">
+                    Read case study
                   </button>
                 </Link>
               </div>
             </div>
 
-            
+
           </div>
         </div>
       </div>
--- src/app/components/examples.js
+++ src/app/components/examples.js
@@ -9,7 +9,7 @@ export default function ExampleGallery() {
             <div className="pt-16 md:pt-16">
                 <div className="container mx-auto max-w-screen-lg">
                     <h1 className="text-center text-2xl font-extrabold leading-none tracking-tight text-gray-100 md:text-3xl xl:text-4xl">Extra Pixels</h1>
-                    <p className="mx-auto max-w-xl text-balance pb-16 pt-4 text-center text-base text-gray-100 dark:text-gray-400">A collection of personal projects.</p>
+                    <p className="mx-auto max-w-xl text-balance pb-16 pt-4 text-center text-base text-gray-100 dark:text-gray-400">Smaller projects and experiments.</p>
                     <div className="grid grid-cols-2 gap-4 px-4 pb-16 md:grid-cols-4">
                         <div className="grid gap-4">
                             <div>
